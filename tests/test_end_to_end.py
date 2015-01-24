d9ebaa1bb50ea3a3162766e90486eb9134715a39
Murad Salahi <salahi.murad@gmail.com>
2015-01-24 12:07:00 -0800

    refactoring to make sure rendered test frame and the image from which it was rendered are coupled together

--- tests/test_end_to_end.py
+++ tests/test_end_to_end.py
@@ -1,6 +1,6 @@
 from PIL import Image
 from catcam.video_stream import VideoStream
-from catcam.curses_image import CursesFrameRenderer
+from catcam.curses_image import CursesFrameRenderer, CursesFrame
 import pytest
 import numpy as np
 import http.server
@@ -8,16 +8,27 @@ from multiprocessing import Process
 import io
 
 
-def tiny_jpeg_bytes():
-    white, black = (255, 255, 255), (0, 0, 0)
-    image = Image.fromarray(np.array([[white, black]], np.uint8))
+def jpeg_encode_image(image):
     jpeg_bytes = io.BytesIO()
     image.save(jpeg_bytes, 'JPEG')
     return jpeg_bytes.getvalue()
 
 
+@pytest.fixture
+def jpeg_and_curses_test_data():
+    data = {}
+    white, black = (255, 255, 255), (0, 0, 0)
+    image = Image.fromarray(np.array([[white, black]], np.uint8))
+    curses_colors, curses_chars = np.array([[15, 16]]), np.array([[' ', '@']])
+    data = {
+        'image': image,
+        'jpeg_bytes': jpeg_encode_image(image),
+        'curses_frame': CursesFrame(curses_chars, curses_colors)}
+    return data
+
+
 class MJPEGRequestHandler(http.server.BaseHTTPRequestHandler):
-    jpeg_bytes = tiny_jpeg_bytes()
+    jpeg_bytes = jpeg_and_curses_test_data()['jpeg_bytes']
     def do_GET(self):
         self.send_response(200)
         self.send_header("Content-type", "image/jpeg")
@@ -32,14 +43,14 @@ def video_stream_server():
     return http.server.HTTPServer(address, MJPEGRequestHandler)
 
     
-def test_end_to_end(video_stream_server):
+def test_end_to_end(video_stream_server, jpeg_and_curses_test_data):
     """
     End-to-end test to ensure that catcam is able to:
         
-        a) stream a jpeg image
+        a) stream a jpeg image over http
         b) map image's colors to xterm-256 colors
         c) map image pixel brightnesses to ascii characters
-        d) image is rendered successfully as a CursesFrame
+        d) successfully render image as a CursesFrame
 
     Test sets up a toy http server process to serve a small jpeg image on GET
     request. Server process is destroyed after streaming a single frame.
@@ -55,8 +66,5 @@ def test_end_to_end(video_stream_server):
     video_stream_server_process.terminate()
     
     # render received image and expected image as curses frames and assert equal.
-    curses_frame_renderer = CursesFrameRenderer((2, 1))
-    curses_frame = curses_frame_renderer.render_frame(frame)
-    test_image = Image.open(io.BytesIO(tiny_jpeg_bytes()))
-    expected_curses_frame = curses_frame_renderer.render_frame(test_image)
-    assert curses_frame == expected_curses_frame
+    curses_frame = CursesFrameRenderer(frame.size).render_frame(frame)
+    assert curses_frame == jpeg_and_curses_test_data['curses_frame']
